169da75206e032d05dbb7d615bea8b00dc395e1b
VapoR <74574116+vaporrrr@users.noreply.github.com>
2022-01-03 23:27:13 -0500

    timezone placeholder

--- src/main/java/com/github/vaporrrr/discordlogger/listeners/BukkitListener.java
+++ src/main/java/com/github/vaporrrr/discordlogger/listeners/BukkitListener.java
@@ -3,6 +3,7 @@ package com.github.vaporrrr.discordlogger.listeners;
 import com.github.vaporrrr.discordlogger.DiscordLogger;
 import github.scarsz.discordsrv.dependencies.jda.api.JDA;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -25,17 +26,21 @@ public class BukkitListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
         if (!discordLogger.getConfig().getBoolean("CommandLog.Enabled")) return;
-        TimeZone.setDefault(TimeZone.getTimeZone(discordLogger.getConfig().getString("CommandLog.TimeZone")));
-        Date now = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        Player player = event.getPlayer();
-        TextChannel channel = jda.getTextChannelById(discordLogger.getConfig().getString("CommandLog.ChannelID"));
+        FileConfiguration config = discordLogger.getConfig();
+        TextChannel channel = jda.getTextChannelById(config.getString("CommandLog.ChannelID"));
         if (channel == null) {
             discordLogger.getLogger().warning("Could not log commands because TextChannel not not found from CommandLog.ChannelID");
             return;
         }
+        String timeZone = config.getString("CommandLog.TimeZone");
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        Player player = event.getPlayer();
+
         String message = discordLogger.getConfig().getString("CommandLog.Format");
         message = message.replace("$time$", format.format(now));
+        message = message.replace("$timezone$", timeZone);
         message = message.replace("$UUID$", player.getUniqueId().toString());
         message = message.replace("$username$", player.getName());
         message = message.replace("$message$", event.getMessage());
